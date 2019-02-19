b8543980525a5c2f498dcfe0757fe4d8422c3130
Paulke29 <paulke29@yahoo.com>
2019-02-19 02:27:07 -0800

    Project1 partial commit

--- src/Driver.java
+++ src/Driver.java
@@ -8,6 +8,9 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.HashMap;
 import java.io.Writer;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Class responsible for running this project based on the provided command-line
@@ -26,34 +29,61 @@ public class Driver {
 	 * 
 	 *
 	 * @param args flag/value pairs used to start this program
-	 */
-	
-	/**  
-	 * get the information from the file which catch from the path and return JSON writing type
-	 * @param inputfile keystem
 	 * @throws IOException 
 	 */
-	public static TreeMap<String,TreeMap<String,TreeSet<Integer>>> output_information(Path inputfile,String Keystem,Writer writer) throws IOException{
-		/**
-		 * return all the clean words from file in the TreeSet
-		 */
-		TreeSet <String>file_information = TextFileStemmer.stemFile(inputfile);
-		for(String stemcompare : file_information) {
-			if(Keystem.equals(stemcompare)) {
-				writer.write("{");
-				PrettyJSONWriter.quote(Keystem, writer);
-			}
-		}
-		return null;
-		
+	/**
+	 * catching valid information from file
+	 * @param args
+	 * @return TreeMap without clean format data 
+	 * @throws IOException
+	 */
+	public static TreeMap<String,TreeMap<String,TreeSet<Integer>>> infor_catching(String[] args) throws IOException  {
+		TreeMap<String,TreeMap<String,TreeSet<Integer>>>infro_catching = new TreeMap<>();
+		Path path;
+        ArgumentMap argumentMap = new ArgumentMap(args);
+//        TextFileStemmer read_file = new TextFileStemmer();
+     /**
+      * checking whether input has "Path" flag or not"
+      */
+        if(argumentMap.hasFlag("-Path") == true) {
+        	/**
+        	 * when "-Path" is true, return the directory of the file 
+        	 */
+        	path = argumentMap.getPath("-Path");
+        	/**
+        	 * loop through the directory 
+        	 */
+        	try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)){
+        		for (Path file : listing) {
+        			if(file.getFileName().toString().toLowerCase().endsWith(".text") || (file.getFileName().toString().toLowerCase().endsWith(".txt"))) {
+        				WordIndex wordindex = new WordIndex();
+        				for(String stemword: TextFileStemmer.stemFile(file)) {
+        					if(!infro_catching.containsKey(stemword)) {
+        						TreeSet<Integer>stemPosition = new TreeSet<>();
+        						
+        						stemPosition.add(3);
+        						infro_catching.put(stemword, new TreeMap<>());
+        					}
+        				}
+        				
+        			}
+        			
+        		}
+        	}
+            System.out.print("Yes");
+        }
+        return infro_catching;
+     
 	}
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws IOException {
 		// store initial start time
 		Instant start = Instant.now();
-
+        infor_catching(args);
 		// TODO Modify this method as necessary.
 		System.out.println(Arrays.toString(args));
-
+		
 		// calculate time elapsed and output
 		Duration elapsed = Duration.between(start, Instant.now());
 		double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
