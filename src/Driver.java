d832cda60308b75b6bc8bced57c9bc236d9f9c96
Paulke29 <paulke29@yahoo.com>
2019-03-11 21:31:56 -0700

    project2

--- src/Driver.java
+++ src/Driver.java
@@ -170,20 +170,7 @@ public class Driver {
 				
 			}
 		}
-		if (argumentMap.hasFlag("-query")) {
-			if (argumentMap.hasValue("-query")) {
-				try {
-					query = argumentMap.getPath("-query");
-					TreeSet<String>querystem = new TreeSet<>();
-					for (String words : TextFileStemmer.stemFile(query)) {
-							querystem.add(words);
-					}
-				} catch (IOException e) {
-					System.out.println(e);
-				}
 
-			}
-		}
 
 
 		System.out.println(Arrays.toString(args));
