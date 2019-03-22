d4eefa23da8e76c7c1cf9a397e9f26e467c532f7
Paulke29 <paulke29@yahoo.com>
2019-03-21 17:50:48 -0700

    Project 2 3/21/2019

--- src/searchResult.java
+++ src/searchResult.java
@@ -9,43 +9,83 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-public class searchResult implements Comparable<searchResult> {
-	private String where;
-	private String score;
-	private int count;
-	private int TotalWords;
-	private String location;
-	public String Queryword;
-	private static TreeMap<String, ArrayList<TreeMap<Object,Object>>> Result;
-	private static ArrayList<TreeMap<Object,Object>> ResultList;
-	private static TreeMap<Object,Object> ResultSearch;
-	private ArrayList<TreeMap<Object,Object>> SearchResultList;
-	private TreeMap<String, String> whereMap;
-	private TreeMap<String, Integer> countMap;
-	private TreeMap<String, Float> scoreMap;
+public class searchResult{
+
 	static WordIndex wordindex = new WordIndex();
 	static int totalword;
 	static String textname;
 	private String QueryWord;
 
 	public searchResult() {
-		Result = new TreeMap<>();
+//		Result = new TreeMap<>();
 //		ResultList = new ArrayList<>();
 //		SearchResultList = new ArrayList<>();
 	}
 
 	/**
-	 * @param file
-	 * @return total
-	 * @throws IOException
+	 * @param where
+	 * @param score
+	 * @param count
+	 * @return WordsResult
 	 */
-	public int getTotal(Path file) throws IOException {
-		int total = 0;
-		for (String words : TextFileStemmer.stemFile(file)) {
-			total++;
+	public TreeMap<Object, Object> WordsResult(String where, String score, int count) {
+		TreeMap<Object, Object> WordsResult = new TreeMap<>();
+		float score2 = Float.parseFloat(score);
+		WordsResult.put("where",where);
+		WordsResult.put("count",count);
+		WordsResult.put("score",score2);
+		return WordsResult;
+	}
+
+	public void updateCount(String file, ArrayList<TreeMap<Object, Object>> getResultList,
+			TreeMap<String, TreeMap<String, TreeSet<Integer>>> filesindex, String QueryWords) {
+		int count = 0;
+		for (TreeMap<Object, Object> treeMap : getResultList) {
+			if (treeMap.containsKey(file)) {
+				count = (int) treeMap.get("count");
+				count += filesindex.get(QueryWords).get(file).size();
+			}
 		}
-//		System.out.println("Total: "+ total);
-		return total;
+	}
+	/**
+	 * @param isExact
+	 * @param QueryWords
+	 * @param wordcounts
+	 * @param filesindex
+	 * @return getResultList
+	 */
+	public ArrayList<TreeMap<Object, Object>> getResult(boolean isExact, String QueryWords,
+			TreeMap<String, Integer> wordcounts, TreeMap<String, TreeMap<String, TreeSet<Integer>>> filesindex) {
+		DecimalFormat df = new DecimalFormat("0.00000000");
+		ArrayList<TreeMap<Object, Object>> getResultList = new ArrayList<>();
+		String where;
+		String score = null;
+		int count = 0;
+		int TotalWords = 0;
+		if (isExact == true) {
+			if (filesindex.containsKey(QueryWords)) {
+				for (String file : filesindex.get(QueryWords).keySet()) {
+					where = file;
+					count = filesindex.get(QueryWords).get(file).size();
+					TotalWords = wordcounts.get(file);
+					score = df.format((float) count / TotalWords);
+					getResultList.add(WordsResult(where, score, count));
+				}
+			}
+		} else {
+			for (String Keys : filesindex.keySet()) {
+				if (Keys.startsWith(QueryWords)) {
+					for (String file : filesindex.get(Keys).keySet()) {
+						where = file;
+						count = filesindex.get(Keys).get(file).size();
+						TotalWords = wordcounts.get(file);
+						score = df.format((float) count / TotalWords);
+						getResultList.add(WordsResult(where, score, count));
+					}
+				}
+			}
+		}
+		return getResultList;
 	}
 
 	public TreeMap<String, ArrayList<TreeMap<Object,Object>>> getSearchResult(boolean isExact, Path query,
@@ -63,77 +103,20 @@ public class searchResult implements Comparable<searchResult> {
 	public TreeMap<String, ArrayList<TreeMap<Object, Object>>> SearchResult(boolean isExact, Path queryfile,
 			TreeMap<String, Integer> wordcounts, TreeMap<String, TreeMap<String, TreeSet<Integer>>> filesindex)
 			throws IOException {
-		DecimalFormat df = new DecimalFormat("0.00000000");
+		ArrayList<TreeMap<Object,Object>> SearchResultList;
+		TreeMap<String, ArrayList<TreeMap<Object,Object>>> Result = new TreeMap<>();
 		for (Set<String> words : TextFileStemmer.QuerystemLine2(queryfile)) {
-			count = 0;
-			TotalWords = 0;
 			if (!words.isEmpty()) {
 				SearchResultList = new ArrayList<>();
-				ResultList = new ArrayList<>();
 				QueryWord = String.join(" ", words);
 				for (String SearchWords : words) {
-					if (!SearchWords.isBlank()) {
-						if (isExact == true) {
-							if (filesindex.containsKey(SearchWords)) {
-								for (String filename : filesindex.get(SearchWords).keySet()) {
-									where = filename;
-									count += filesindex.get(SearchWords).get(filename).size();
-									for (String Namefile : wordcounts.keySet()) {
-										if (filename.equals(Namefile)) {
-											TotalWords = wordcounts.get(filename);
-										}
-									}
-									score = df.format((float) count / TotalWords);
-								}
-							}
-						} else {
-							for (String Keys : filesindex.keySet()) {
-								if (Keys.startsWith(SearchWords)) {
-									for (String filename : filesindex.get(Keys).keySet()) {
-										where = filename;
-										count += filesindex.get(Keys).get(filename).size();
-										for (String Namefile : wordcounts.keySet()) {
-											if (filename.equals(Namefile)) {
-												TotalWords = wordcounts.get(filename);
-											}
-										}
-										score = df.format((float) count / TotalWords);
-									}
-								}
-							}
-						}
-					}
+					SearchResultList.addAll(getResult(isExact, SearchWords,wordcounts, filesindex));
 				}
-				if (count > 0) {
-					ResultSearch = new TreeMap<>();
-					ResultSearch.put("where", where);
-					ResultSearch.put("count", count);
-					float score2 = Float.parseFloat(score);
-					ResultSearch.put("score", score2);
-					SearchResultList.add(ResultSearch);
-					ResultList.addAll(SearchResultList);
-				} else {
-					ResultSearch = null;
-				}
-				if (Result.containsKey(QueryWord)) {
-					Result.get(QueryWord).add(ResultSearch);
-				}
-				if (!Result.containsKey(QueryWord)) {
-//						SearchResultList.add(ResultSearch);
-						Result.put(QueryWord, ResultList);
-				}
+				Result.put(QueryWord, SearchResultList);
 			}
 		}
 		return Result;
 	}
-
-	public String toString() {
-		return "QueryWord:" + Queryword + " " + "where:" + location + " " + "count:" + count + " " + "score:" + score;
-
-	}
-
-	@Override
-	public int compareTo(searchResult other) {
-		return this.score.compareTo(other.score);
-	}
+//	public TreeMap<String, ArrayList<TreeMap<Object, Object>>> SortResult()
+	
 }
